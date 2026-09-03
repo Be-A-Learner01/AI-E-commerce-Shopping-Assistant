75d6a889c027e7a55bcaf4ea72e686839967252d
Be-A-Learner01 <a3442302652@163.com>
2026-09-04 01:24:29 +0800

    add langsmith to trace agent

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from app.agent.graph import agent
-from langchain_core.messages import HumanMessage
+from dotenv import load_dotenv
 import asyncio
-query=("我想买一个4000元内的平板电脑")
+
+load_dotenv()
+
+query="我想买一个4000元内的平板电脑"
 async def main():
     answer = await agent.ainvoke(
         {"query":query}
